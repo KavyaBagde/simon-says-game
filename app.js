2e20e2bdcccb5d3e2060da62fdd497d20f831c7c
Kavya Bagde <kavyabagde0606@gmail.com>
2024-10-02 07:41:13 +0530

    change revert

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ let heighScore = 0;
 let h2 = document.querySelector("h2");
 
 document.addEventListener("click", function (key) {
- 
+ if(key.code == "space"){
     if (started == false) {
         started = true;
         levelUp();
     }
-  
+  }
 });
 
 function btnFlash(btn){
